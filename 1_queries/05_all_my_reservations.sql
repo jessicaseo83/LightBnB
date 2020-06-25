b9b4e00c97325cb7fa242837a602251c5efbee9f
Jessica Seo <jessicaseo83@gmail.com>
2020-06-25 00:21:30 +0000

    Modified query

--- 1_queries/05_all_my_reservations.sql
+++ 1_queries/05_all_my_reservations.sql
@@ -1,7 +1,7 @@
 SELECT reservations.* , properties.*, AVG(property_reviews.rating)
 FROM reservations
-JOIN properties ON properties.id = property_id
-JOIN property_reviews ON reservation_id = reservations.id
+JOIN properties ON properties.id = reservations.property_id
+JOIN property_reviews ON properties.id = property_reviews.property_id
 WHERE reservations.guest_id = 1 AND reservations.end_date < now()::date
 GROUP BY reservations.id, properties.id
 ORDER BY reservations.start_date
